b64508a1c3f6558e305a8282391a31363f0d0706
John Morkel <mmmorks@mmmorks.net>
2024-04-05 00:07:17 -0700

    Add back case insensitivity to decryption search

--- format/base.py
+++ format/base.py
@@ -64,10 +64,10 @@ class Base(object):
         print(search_value)
         print(search_value_padded)
 
-        search_exact = re.compile('.*'+search_value+'.*', flags=re.MULTILINE|re.DOTALL)
+        search_exact = re.compile('.*'+search_value+'.*', flags=re.IGNORECASE|re.MULTILINE|re.DOTALL)
         # sometimes there is an extra character after each character
         # 37805-RBB-J530 -> 3377880550--RRBCBA--JA503000
-        search_padded = re.compile('.*'+search_value_padded+'.*', flags=re.MULTILINE|re.DOTALL)
+        search_padded = re.compile('.*'+search_value_padded+'.*', flags=re.IGNORECASE|re.MULTILINE|re.DOTALL)
         operators = [
             { 'fn': operator.__xor__, 'sym': '^' },
             { 'fn': operator.__and__, 'sym': '&' },
@@ -83,7 +83,7 @@ class Base(object):
         for i in range(len(self._keys)):
             k = ord(self._keys[i])
             keys.append({ 'val': k, 'sym': 'k{}'.format(i) })
-        assert len(keys) == 3, "excatly three keys currently required!"
+        assert len(keys) == 3, "exactly three keys currently required!"
 
         firmware_candidates = list()
 
